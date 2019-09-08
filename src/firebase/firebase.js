72a316f350706f6033c4fbf2743204b53408d9e0
Richard Whiteside <rwhiteside4@gmail.com>
2019-09-08 14:10:43 +0100

    Setup Test DB environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,25 +1,38 @@
 import * as firebase from 'firebase'
 
+
+
+// const config = {
+//   apiKey: AIzaSyD8upzGpvjiY5Il2C7oNVCJ7mHwMQCYZLE,
+//   authDomain: "expensify-1b6e9.firebaseapp.com",
+//   databaseURL: "https://expensify-1b6e9.firebaseio.com",
+//   projectId: "expensify-1b6e9",
+//   storageBucket: "expensify-1b6e9.appspot.com",
+//   messagingSenderId: "864798334319",
+//   appId: "1:864798334319:web:106f2952006df46de99c9a"
+// }
+
+
 const config = {
-  apiKey: "AIzaSyD8upzGpvjiY5Il2C7oNVCJ7mHwMQCYZLE",
-  authDomain: "expensify-1b6e9.firebaseapp.com",
-  databaseURL: "https://expensify-1b6e9.firebaseio.com",
-  projectId: "expensify-1b6e9",
-  storageBucket: "expensify-1b6e9.appspot.com",
-  messagingSenderId: "864798334319",
-  appId: "1:864798334319:web:106f2952006df46de99c9a"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 
 firebase.initializeApp(config);
 
 const database = firebase.database()
 
+export {firebase, database as default }
+
 // database().ref().set({
 //   name: 'Barney Boy',
 //   age: 7,
 //   isSingle: true
 // })
-
 
 
 // const del = [{
@@ -78,34 +91,34 @@ const database = firebase.database()
 //   console.log(expenses);
 // })
 
-database.ref('expenses')
-.on('value',  snapshot => {
-  const expenses = []
-  snapshot.forEach(childSnapshot => {
-    expenses.push({ id: childSnapshot.key, ...childSnapshot.val()})
-  })
-  console.log(expenses)
-})
+// database.ref('expenses')
+// .on('value',  snapshot => {
+//   const expenses = []
+//   snapshot.forEach(childSnapshot => {
+//     expenses.push({ id: childSnapshot.key, ...childSnapshot.val()})
+//   })
+//   console.log(expenses)
+// })
 
-database.ref('expenses')
-.on('child_removed', snapshot => {
-  console.log(snapshot.key, snapshot.val());
-})
+// database.ref('expenses')
+// .on('child_removed', snapshot => {
+//   console.log(snapshot.key, snapshot.val());
+// })
 
-database.ref('expenses')
-.on('child_added', snapshot => {
-  console.log(snapshot.key, snapshot.val());
-})
+// database.ref('expenses')
+// .on('child_added', snapshot => {
+//   console.log(snapshot.key, snapshot.val());
+// })
 
-database.ref('expenses')
-.on('child_changed', snapshot => {
-  console.log(snapshot.key, snapshot.val());
-})
+// database.ref('expenses')
+// .on('child_changed', snapshot => {
+//   console.log(snapshot.key, snapshot.val());
+// })
 
 
-setTimeout( () => {
-  database.ref('expenses').push({
-    name: "Barney",
-    age: "6.5"
-  })
-},5000)
+// setTimeout( () => {
+//   database.ref('expenses').push({
+//     name: "Barney",
+//     age: "6.5"
+//   })
+// },5000)
